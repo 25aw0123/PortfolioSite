e68891dc106372c3911b87fc1dcbe96a8498661e
TzuChien Chang <25aw0123@jec.ac.jp>
2026-02-12 02:00:46 +0900

    feat: works sectionを完成

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  
   const hamburger = document.querySelector('.menu-button');
   const nav = document.querySelector('.global-nav');
 
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.body.style.overflow = isOpen ? 'hidden' : '';
   });
+
 
   const scrollBtn = document.getElementById('back-to-top');
   window.addEventListener('scroll', () => {
@@ -28,8 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  const accordions = document.querySelectorAll('.accordion-header');
 
+  const accordions = document.querySelectorAll('.accordion-header');
   accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
       accordion.classList.toggle('active');
@@ -41,4 +43,62 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+  
+
+  const worksData = [
+    {
+      id: 'recipe-owned-media',
+      category: '学校課題',
+      title: 'レシピオウンドメディア',
+      thumbnail: './img/work01_recipe_ownedmedia/work01_thumbnail.png'
+    },
+    {
+      id: 'my-portfolio',
+      category: '自主制作',
+      title: '個人ポートフォリオ',
+      thumbnail: './img/work02_portfolio/work02_thumbnail.png'
+    },
+    {
+      id: 'photo-search-app',
+      category: '学校課題',
+      title: '画像検索アプリ（WebAPIを利用）',
+      thumbnail: './img/work03_photo_search_app/work03_thumbnail.png'
+    }
+  ];
+
+  const categoryColors = {
+    '学校課題': '#7CABC5',
+    '自主制作': '#e18546'
+  };
+
+  const renderWorks = () => {
+    const container = document.getElementById('works-list');
+    if (!container) return;
+
+    let htmlContent = '';
+
+    worksData.forEach(item => {
+      const tagColor = categoryColors[item.category];
+
+      htmlContent += `
+            <a href="./works/${item.id}/index.html" class="work-card" target=_blank>
+
+                <div class="work-card__content">
+                    <p class="work-card__category" style="color: ${tagColor}; border-color: ${tagColor};">
+                        ${item.category}
+                    </p>
+                    <h3 class="work-card__title">${item.title}</h3>
+                </div>
+                
+                <div class="work-card__thumbnail">
+                    <img src="${item.thumbnail}" alt="${item.title}" loading="lazy">
+                </div>
+
+                <span class="work-card__arrow">→</span>
+            </a>
+        `;
+    });
+    container.innerHTML = htmlContent;
+  }
+  renderWorks();
 });
